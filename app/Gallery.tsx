da505f113c89e63006ccfd30ee6d5a45803b4176
Muflih-uk <muhammedmuflih875@gmail.com>
2025-03-22 19:57:57 +0530

    Pictures Uploaded in Gallery Page

--- app/Gallery.tsx
+++ app/Gallery.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useRef } from "react";
+import Image from "next/image";
 
 const Gallery = () => {
   const scrollRef = useRef<HTMLDivElement>(null);
@@ -16,14 +17,19 @@ const Gallery = () => {
        ref={scrollRef}
        onWheel={handleScroll}
        className="flex p-4 pl-4 space-x-4 overflow-x-auto w-[95%] h-[600px] scrollbar-hide">
-        <div className="flex items-center justify-center p-[30px] flex-nowrap space-x-[30px]">
-          <div className="w-[400px] h-[400px] bg-black rounded-[30px] hover:scale-105 hover:rotate-8"></div>
-          <div className="w-[400px] h-[400px] bg-black rounded-[30px] hover:scale-105 hover:rotate-8"></div>
-          <div className="w-[400px] h-[400px] bg-black rounded-[30px] hover:scale-105 hover:rotate-8"></div>
-          <div className="w-[400px] h-[400px] bg-black rounded-[30px] hover:scale-105 hover:rotate-8"></div>
-          <div className="w-[400px] h-[400px] bg-black rounded-[30px] hover:scale-105 hover:rotate-8"></div>
-          <div className="w-[400px] h-[400px] bg-black rounded-[30px] hover:scale-105 hover:rotate-8"></div>
-          <div className="w-[400px] h-[400px] bg-black rounded-[30px] hover:scale-105 hover:rotate-8"></div>
+        <div className="flex items-center justify-center p-[30px] flex-nowrap space-x-[30px] min-w-max">
+          <Image src={'/2.png'} width={400} height={400} alt="2" className="rounded-[30px] hover:scale-105 hover:rotate-8"/>
+          <Image src={'/3.png'} width={400} height={400} alt="3" className="rounded-[30px] hover:scale-105 hover:rotate-8"/>
+          <Image src={'/4.png'} width={400} height={400} alt="4" className="rounded-[30px] hover:scale-105 hover:rotate-8"/>
+          <Image src={'/5.png'} width={400} height={400} alt="5" className="rounded-[30px] hover:scale-105 hover:rotate-8"/>
+          <Image src={'/6.png'} width={400} height={400} alt="6" className="rounded-[30px] hover:scale-105 hover:rotate-8"/>
+          <Image src={'/7.png'} width={400} height={400} alt="7" className="rounded-[30px] hover:scale-105 hover:rotate-8"/>
+          <Image src={'/8.png'} width={400} height={400} alt="8" className="rounded-[30px] hover:scale-105 hover:rotate-8"/>
+          <Image src={'/9.png'} width={400} height={400} alt="9" className="rounded-[30px] hover:scale-105 hover:rotate-8"/>
+          <Image src="/10.png" width={400} height={400} alt="10" className="rounded-[30px] hover:scale-105 hover:rotate-8" />
+          <Image src="/11.png" width={400} height={400} alt="11" className="rounded-[30px] hover:scale-105 hover:rotate-8" />
+          <Image src="/12.png" width={400} height={400} alt="12" className="rounded-[30px] hover:scale-105 hover:rotate-8" />
+          <Image src="/13.png" width={400} height={400} alt="13" className="rounded-[30px] hover:scale-105 hover:rotate-8" />
         </div>
       </div>
     </section>
