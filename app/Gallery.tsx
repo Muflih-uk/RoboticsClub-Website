c3c7a147b865d5757ef977bc231e6e99db74c5b2
Muflih-uk <muhammedmuflih875@gmail.com>
2025-03-22 22:52:43 +0530

    Pictures Mobile Setup in Gallery Page

--- app/Gallery.tsx
+++ app/Gallery.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Gallery = () => {
   const scrollRef = useRef<HTMLDivElement>(null);
-
+  const photos = ["/2.png","/3.png","/4.png","/5.png","/6.png","/7.png","/8.png","/9.png","/10.png","/11.png","/12.png","/13.png"]
   const handleScroll = (event: React.WheelEvent) => {
     if (scrollRef.current) {
       scrollRef.current.scrollLeft += event.deltaY;
@@ -17,19 +17,12 @@ const Gallery = () => {
        ref={scrollRef}
        onWheel={handleScroll}
        className="flex p-4 pl-4 space-x-4 overflow-x-auto w-[95%] h-[600px] scrollbar-hide">
-        <div className="flex items-center justify-center p-[30px] flex-nowrap space-x-[30px] min-w-max">
-          <Image src={'/2.png'} width={400} height={400} alt="2" className="rounded-[30px] hover:scale-105 hover:rotate-8"/>
-          <Image src={'/3.png'} width={400} height={400} alt="3" className="rounded-[30px] hover:scale-105 hover:rotate-8"/>
-          <Image src={'/4.png'} width={400} height={400} alt="4" className="rounded-[30px] hover:scale-105 hover:rotate-8"/>
-          <Image src={'/5.png'} width={400} height={400} alt="5" className="rounded-[30px] hover:scale-105 hover:rotate-8"/>
-          <Image src={'/6.png'} width={400} height={400} alt="6" className="rounded-[30px] hover:scale-105 hover:rotate-8"/>
-          <Image src={'/7.png'} width={400} height={400} alt="7" className="rounded-[30px] hover:scale-105 hover:rotate-8"/>
-          <Image src={'/8.png'} width={400} height={400} alt="8" className="rounded-[30px] hover:scale-105 hover:rotate-8"/>
-          <Image src={'/9.png'} width={400} height={400} alt="9" className="rounded-[30px] hover:scale-105 hover:rotate-8"/>
-          <Image src="/10.png" width={400} height={400} alt="10" className="rounded-[30px] hover:scale-105 hover:rotate-8" />
-          <Image src="/11.png" width={400} height={400} alt="11" className="rounded-[30px] hover:scale-105 hover:rotate-8" />
-          <Image src="/12.png" width={400} height={400} alt="12" className="rounded-[30px] hover:scale-105 hover:rotate-8" />
-          <Image src="/13.png" width={400} height={400} alt="13" className="rounded-[30px] hover:scale-105 hover:rotate-8" />
+        <div className="flex items-center justify-center md:p-[30px] flex-nowrap space-x-[30px] min-w-max">
+          {
+            photos.map((item,index)=>(
+              <Image key={index} src={item} width={300} height={300} alt={item} className="md:w-[400px] md:h-[400px] rounded-[30px] hover:scale-105 hover:rotate-8"/>
+            ))
+          }
         </div>
       </div>
     </section>
